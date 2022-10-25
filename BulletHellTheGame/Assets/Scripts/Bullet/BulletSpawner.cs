91eb5150a190d7ea7e0d4a20d1780b9c441dcfc6
Snaksiu <kewin_mejer@wp.pl>
2022-10-25 17:28:31 +0200

    Day 2 UNITY PAIN

--- BulletHellTheGame/Assets/Scripts/Bullet/BulletSpawner.cs
+++ BulletHellTheGame/Assets/Scripts/Bullet/BulletSpawner.cs
@@ -4,24 +4,23 @@ using UnityEngine;
 
 public class BulletSpawner : MonoBehaviour
 {
-    public GameObject bulletResource;
-    public float minRotation;
-    public float maxRotation;
-    public int NumberOfBullets;
-    public bool isRandom;
+    public BulletSpawnData[] spawnDatas;
+    public int index = 0;
+    public bool isSequenceRandom;
 
-    public float cooldown;
+    BulletSpawnData GetSpawnData()
+    {
+        return spawnDatas[index];
+    }
     float timer;
-    public float bulletSpeed;
-    public Vector2 bulletVelocity;
 
     float[] rotations;
     void Start()
     {
-        timer = cooldown;
-        rotations = new float[NumberOfBullets];
+        timer = GetSpawnData().cooldown;
+        rotations = new float[GetSpawnData().NumberOfBullets];
 
-        if (!isRandom)
+        if (!GetSpawnData().isRandom)
         {
             DistributedRotations();
         }
@@ -33,48 +32,72 @@ public class BulletSpawner : MonoBehaviour
         if(timer <= 0)
         {
             SpawnBullets();
-            timer = cooldown;
+            timer = GetSpawnData().cooldown;
+            if (isSequenceRandom)
+            {
+                index = Random.Range(0, spawnDatas.Length);
+            }
+            else
+            {
+                index += 1;
+                if (index >= spawnDatas.Length) index = 0;
+            }
         }
         timer -= Time.deltaTime;
     }
     //gives a random rotation from min to max for each bullet.
     public float[] RandomRotations()
     {
-        for (int i = 0; i < NumberOfBullets; i++)
+        for (int i = 0; i < GetSpawnData().NumberOfBullets; i++)
         {
-            rotations[i] = Random.Range(minRotation, maxRotation);
+            rotations[i] = Random.Range(GetSpawnData().minRotation, GetSpawnData().maxRotation);
         }
         return rotations;
     }
     //gives a random rotations evenly distributed between min and max rotation.
     public float[] DistributedRotations()
     {
-        for (int i = 0; i < NumberOfBullets; i++)
+        for (int i = 0; i < GetSpawnData().NumberOfBullets; i++)
         {
-            var fraction = (float)i / (float)NumberOfBullets;
-            var difference = maxRotation - minRotation;
+            var fraction = (float)i / (float)GetSpawnData().NumberOfBullets;
+            var difference = GetSpawnData().maxRotation - GetSpawnData().minRotation;
             var fractionOfDifference = fraction * difference;
-            rotations[i] = fractionOfDifference + minRotation;
+            rotations[i] = fractionOfDifference + GetSpawnData().minRotation;
         }
         foreach (var r in rotations) print(r);
         return rotations;
     }
     public GameObject[] SpawnBullets()
     {
-        if (isRandom)
+        if (GetSpawnData().isRandom)
         {
             //this is in void update cuz we want random rotations for each new bullet
             RandomRotations();
         }
-            //spawnbullet
-        GameObject[] spawnedBullets = new GameObject[NumberOfBullets];
-        for (int i = 0; i < NumberOfBullets; i++)
+            //spawn bullets
+        GameObject[] spawnedBullets = new GameObject[GetSpawnData().NumberOfBullets];
+        for (int i = 1; i < GetSpawnData().NumberOfBullets; i++)
         {
-            spawnedBullets[i] = Instantiate(bulletResource, transform);
+            spawnedBullets[i] = BulletManager.GetBulletFromPool();
+            if(spawnedBullets[i] == null)
+            {
+                spawnedBullets[i] = Instantiate(GetSpawnData().bulletResource, transform);
+            }else
+            {
+                spawnedBullets[i].transform.SetParent(transform);
+                spawnedBullets[i].transform.localPosition = Vector2.zero;
+            }
+
+
             var b = spawnedBullets[i].GetComponent<Bullet>();
-            b.rotation = rotations[i];
-            b.speed = bulletSpeed;
-            b.velocity = bulletVelocity;
+            if (i < GetSpawnData().NumberOfBullets)
+            {
+                b.rotation = rotations[i];
+            }
+            b.speed = GetSpawnData().bulletSpeed;
+            b.velocity = GetSpawnData().bulletVelocity;
+            if (GetSpawnData().isNotParent) spawnedBullets[i].transform.SetParent(null);
+
         }
         return spawnedBullets;
     }
